5f84738367ca7ef5c67b6d36f43dbd59b9f13924
Martin Kruger <martin@martinkruger.com>
2021-04-23 16:07:56 +0200

    Feat: Layout

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import * as S from './Header.style';
 
+type Props = {
+  logo: string;
+};
+
 /**
  * The main header for the site
  */
-function Header() {
-  return <S.Wrapper>Header</S.Wrapper>;
+function Header({ logo }: Props) {
+  return (
+    <S.Wrapper>
+      <S.LogoContainer>
+        <S.Logo src={logo} alt="" />
+        Digital Filing
+      </S.LogoContainer>
+      <S.NavMenu />
+    </S.Wrapper>
+  );
 }
 
 export default Header;
