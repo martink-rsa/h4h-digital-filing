5dcc34d393f19162b137e59a4202834dc8b0c64e
Martin Kruger <martin@martinkruger.com>
2021-04-23 13:24:45 +0200

    dev branch init

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import * as S from './App.style';
+import FileUpload from '../FileUpload';
 
 function App() {
-  return <S.Wrapper>Setup complete</S.Wrapper>;
+  return (
+    <S.Wrapper>
+      Setup complete
+      <FileUpload />
+    </S.Wrapper>
+  );
 }
 
 export default App;
---
+++ client/src/components/FileUpload/FileUpload.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as S from './FileUpload.style';
+
+function FileUpload() {
+  return <div>File Upload</div>;
+}
+
+export default FileUpload;
