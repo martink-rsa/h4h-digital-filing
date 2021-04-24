bb56117b4d99123ce81359ab44c74e57915c5f74
Nic Saal <slxnic001@myuct.ac.za>
2021-04-24 14:20:46 +0200

    Created controller for upload/download

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,12 @@
 import * as express from 'express';
+import controller from 'controller';
 
 const app = express();
 
 app.use(express.json());
+
+app.post("/upload", controller.upload);
+app.get("/files", controller.getListFiles);
+app.get("/files/:name", controller.download);
 
 export default app;
--- server/src/controller/file.controller.js
+++ server/src/controller/file.controller.js
@@ -12,6 +12,48 @@ const upload = async (req, res) => {
         res.status(200).send({message: "Upload successful:" + req.file.originalname,})
     } catch (err)
     {
-        res.status(500).send({message: 'Error uploading file:" ${err}',});
+        if(err.code == "LIMIT_FILE_SIZE"){
+            return res.status(500).send({message: "File size cannot be larger than 10MB",})
+        }
+        
+        res.status(500).send({message: 'Cannot upload file. Error:' + err.message,});
     }
+};
+
+const getListFiles = (req, res) => {
+    const diretoryPath = __basedir + "/resources/static/assets/uploads";
+
+    fs.readdir(diretoryPath, function(err, files){
+        if (err){
+            res.status(500).send({message:"Cannot get file list. Error:" + err.message,});
+        }
+
+        let fileInfo = [];
+        
+        files.forEach((file) => {
+            fileInfos.push({
+                name: file,
+                url: baseUrl + file,
+            });
+        });
+
+        res.status(200).send(fileInfos);
+        });
+    };
+
+    const download = (req, res) => {
+    const fileName = req.params.name;
+    const directoryPath = __basedir + "/resources/static/assets/uploads/";
+
+    res.dowload(directoryPath + fileName, (err) => {
+        if(err){
+            res.status(500).send({ message: "Cannot download the file. Error: " + err.message,});
+        }
+    });
+};
+
+module.exports = {
+    upload,
+    getListFiles,
+    download,
 };
