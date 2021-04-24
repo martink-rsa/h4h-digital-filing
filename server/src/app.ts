fb4b24a01fce514450dd7e5961f50a5522a22e27
GraemeHarris <graeme@mohara.co>
2021-04-24 15:19:58 +0200

    [FIXED] Import for Nic :)

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,12 @@
 import * as express from 'express';
-import controller from 'controller';
 
+const fileController = require("./controller/file.controller")
 const app = express();
 
 app.use(express.json());
 
-app.post("/upload", controller.upload);
-app.get("/files", controller.getListFiles);
-app.get("/files/:name", controller.download);
+app.post("/upload", fileController.upload);
+app.get("/files", fileController.getListFiles);
+app.get("/files/:name", fileController.download);
 
 export default app;
